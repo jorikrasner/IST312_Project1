461ead35b0e070d5a0a762f205d74c083ee9cfc6
Jori Krasner <kraznerj@siu.edu>
2015-02-25 22:04:24 -0600

    first three classes

---
+++ src/mw_jk_project_01/manager.java
@@ -0,0 +1,8 @@
+package mw_jk_project_01;
+
+public class manager {
+	//Attributes
+	private int editor;
+	private int manager;
+	
+}
---
+++ src/mw_jk_project_01/user.java
@@ -0,0 +1,87 @@
+package mw_jk_project_01;
+
+public class user {
+	//Attributes
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String password;
+	/**
+	 * @param firstname
+	 * @param lastname
+	 * @param email
+	 * @param password
+	 */
+	public user(String firstname, String lastname, String email, String password) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public user(user object2){
+		this.firstname = object2.firstname;
+		this.lastname = object2.lastname;
+		this.email = object2.email;
+		this.password = object2.password;
+	}
+	
+	/**
+	 * @return the firstname
+	 */
+	public String getFirstname() {
+		return firstname;
+	}
+	/**
+	 * @param firstname the firstname to set
+	 */
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	/**
+	 * @return the lastname
+	 */
+	public String getLastname() {
+		return lastname;
+	}
+	/**
+	 * @param lastname the lastname to set
+	 */
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return firstname + lastname + email;
+	}
+	
+	
+	
+}
