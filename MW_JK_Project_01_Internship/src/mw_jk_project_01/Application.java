d5ed8d5cbcbc08c68bdc73247d6b6c09c10d8b8e
Jori Krasner <kraznerj@siu.edu>
2015-03-01 15:37:41 -0600

    updated new classes

---
+++ MW_JK_Project_01_Internship/src/mw_jk_project_01/Application.java
@@ -0,0 +1,74 @@
+public class Application {
+	private String internshipapp;
+	private String coverletter;
+	private String dateofsubbmission;
+	private String internposition;
+	private Internship internship;
+	private Resume resume;
+	private Object attribute;
+	public Applicants unnamed_Applicants_;
+
+	public String getInternshipapp() {
+		return this.internshipapp;
+	}
+
+	public void setInternshipapp(String internshipapp) {
+		this.internshipapp = internshipapp;
+	}
+
+	public String getcoverletter() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setcoverletter(String coverletter) {
+		throw new UnsupportedOperationException();
+	}
+
+	public String getDateofsubbmission() {
+		return this.dateofsubbmission;
+	}
+
+	public void setDateofsubbmission(String dateofsubbmission) {
+		this.dateofsubbmission = dateofsubbmission;
+	}
+
+	public String getInternposition() {
+		return this.internposition;
+	}
+
+	public void setInternposition(String internposition) {
+		this.internposition = internposition;
+	}
+
+	public void getInternshipInformation() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void toString() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Internship getInternship() {
+		return this.internship;
+	}
+
+	public void setInternship(Internship internship) {
+		this.internship = internship;
+	}
+
+	public Resume getResume() {
+		return this.resume;
+	}
+
+	public void setResume(Resume resume) {
+		this.resume = resume;
+	}
+
+	public void getAttribute() {
+		return this.attribute;
+	}
+
+	public void setAttribute(Object attribute) {
+		this.attribute = attribute;
+	}
+}
---
+++ MW_JK_Project_01_Internship/src/mw_jk_project_01/Company.java
@@ -0,0 +1,56 @@
+public class Company {
+	private String companyName;
+	private String city;
+	private String zip;
+	private String state;
+	private String phone;
+	private String url;
+
+	public String getCompanyName() {
+		return this.companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getCity() {
+		return this.city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZip() {
+		return this.zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getState() {
+		return this.state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPhone() {
+		return this.phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getUrl() {
+		return this.url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
